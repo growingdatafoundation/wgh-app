e8e3495fec02034ba64b3d904ba79b87bac9e1b8
Jakob Murko <jakob.murko@gmail.com>
2017-12-15 02:04:33 +0100

    Fixes

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ var mylocation = [138.599899, -34.9274284]
 var myregion = ''
 // var mylocation=[0,140.9987875413551];
 var isConnection = true
-var URL = 'http://localhost:8080/'
+var URL = 'https://awsgdf01.growingdatafoundation.org.au/api/'
 var totalplan = 0
 var osm = 'http://nominatim.openstreetmap.org/search?format=json&addressdetails=1&limit=1'
 var ls_data_list
@@ -64,17 +64,17 @@ function init_map () {
   map = new ol.Map({
     target: 'locationMap',
     layers: [
-		  new ol.layer.Tile({
-			 source: new ol.source.OSM()
-		  })
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
     ],
-		 view: new ol.View({
-   center: ol.proj.transform([138.599899, -34.9274284], 'EPSG:4326', 'EPSG:3857'),
-   zoom: 14
-		  })
+    view: new ol.View({
+      center: ol.proj.transform([138.599899, -34.9274284], 'EPSG:4326', 'EPSG:3857'),
+      zoom: 14
+    })
   })
   var vectorLayer = new ol.layer.Vector({
-	  source: vectorSource
+    source: vectorSource
   })
   map.addLayer(vectorLayer)
   setMarker(mylocation)
@@ -86,21 +86,21 @@ function init_map () {
 }
 
 function setMarker (lonlat) {
-	 var iconFeature = new ol.Feature({
-	  geometry: new ol.geom.Point(new ol.proj.transform(lonlat, 'EPSG:4326', 'EPSG:3857')),
-	  // geometry: new ol.proj.transform([138.599899, -34.9274284], 'EPSG:4326', 'EPSG:3857'),
-	  name: 'location'
- })
+  var iconFeature = new ol.Feature({
+    geometry: new ol.geom.Point(new ol.proj.transform(lonlat, 'EPSG:4326', 'EPSG:3857')),
+    // geometry: new ol.proj.transform([138.599899, -34.9274284], 'EPSG:4326', 'EPSG:3857'),
+    name: 'location'
+  })
 
   var iconStyle = new ol.style.Style({
-	  image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-    anchor: [0.5, 70],
-    anchorXUnits: 'fraction',
-    anchorYUnits: 'pixels',
-    opacity: 0.9,
-    scale: 0.5,
-    src: 'img/app_icons/location_pin.png'
-	  }))
+    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+      anchor: [0.5, 70],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'pixels',
+      opacity: 0.9,
+      scale: 0.5,
+      src: 'img/app_icons/location_pin.png'
+    }))
   })
 
   iconFeature.setStyle(iconStyle)
@@ -134,10 +134,10 @@ function find_address () {
 
   $.post(osm + '&q=' + encodeURI($('#address').val()), {}, function (data) {
     $.mobile.loading('hide')
-		// alert(JSON.stringify(data));
+    // alert(JSON.stringify(data));
     if (data.length > 0) {
       mylocation = [parseFloat(data[0].lon), parseFloat(data[0].lat)]
-			// alert(JSON.stringify(mylocation));
+      // alert(JSON.stringify(mylocation));
       setMarker(mylocation)
       map.getView().setCenter(ol.proj.transform(mylocation, 'EPSG:4326', 'EPSG:3857'))
     }
@@ -158,25 +158,25 @@ function click_landscape () {
     $.mobile.loading('hide')
     var str = ''
     for (var i = 0; i < data.plants.length; i++) {
-			 var formimg = '<img src="img/app_icons/nothumbnail.png">'
+      var formimg = '<img src="img/app_icons/nothumbnail.png">'
 
-			 if (typeof data.plants[i].imageUrl !== 'undefined' && data.plants[i].imageUrl != '') {
-   formimg = '<img src="' + data.plants[i].imageUrl + '">'
-			 }
+      if (typeof data.plants[i].imageUrl !== 'undefined' && data.plants[i].imageUrl != '') {
+        formimg = '<img src="' + data.plants[i].imageUrl + '">'
+      }
 
-			 var starimg = '<img class="fav-icon-list" src="img/app_icons/star_off.png" onclick="add_ls_list(\'' + data.plants[i].scientificName + '\',this)"/>'
-			 for (var j = 0; j < ls_fav_list.length; j++) {
-   if (ls_fav_list[j].scientificName == data.plants[i].scientificName) {
-					 starimg = '<img class="fav-icon-list" src="img/app_icons/star_on.png" onclick="remove_ls_list(\'' + data.plants[i].scientificName + '\',this)"/>'
-   }
-			 }
+      var starimg = '<img class="fav-icon-list" src="img/app_icons/star_off.png" onclick="add_ls_list(\'' + data.plants[i].scientificName + '\',this)"/>'
+      for (var j = 0; j < ls_fav_list.length; j++) {
+        if (ls_fav_list[j].scientificName == data.plants[i].scientificName) {
+          starimg = '<img class="fav-icon-list" src="img/app_icons/star_on.png" onclick="remove_ls_list(\'' + data.plants[i].scientificName + '\',this)"/>'
+        }
+      }
 
-			 var occurrences = ''
-			 if (typeof data.plants[i].count !== 'undefined' && data.plants[i].count != '') {
-   occurrences = ' - Occurrences: ' + data.plants[i].count
-			 }
+      var occurrences = ''
+      if (typeof data.plants[i].count !== 'undefined' && data.plants[i].count != '') {
+        occurrences = ' - Occurrences: ' + data.plants[i].count
+      }
 
-			 str += '<li onclick="open_detail(\'' + data.plants[i].scientificName + '\',\'list\',\'ls\')">' + formimg + '<h3>' + data.plants[i].scientificName + '</h3><p>' + data.plants[i].commonName + occurrences + '</p>' + starimg + '</li>'
+      str += '<li onclick="open_detail(\'' + data.plants[i].scientificName + '\',\'list\',\'ls\')">' + formimg + '<h3>' + data.plants[i].scientificName + '</h3><p>' + data.plants[i].commonName + occurrences + '</p>' + starimg + '</li>'
     }
     if (typeof data.region !== 'undefined' && data.region != '') {
       $('.region-name-header').html(' - ' + data.region.name + ' - ' + data.region.regionName + ' - ' + data.region.state)
@@ -191,6 +191,28 @@ function click_landscape () {
     $.mobile.loading('hide')
   })
 }
+
+function getForm (type) {
+  switch (type) {
+    case 'Trees and Shrubs':
+      return '1'
+    case 'Groundcover':
+      return '2'
+    case 'Climbers':
+      return '8'
+    case 'Grasses':
+      return '10'
+    case 'Other Strap-leaved Plants':
+      return '9'
+    case 'Bulbs and Lilies':
+      return '7'
+    case 'Aquatic and Riparian Zone Plants':
+      return '4'
+    case 'Rushes and Sedges':
+      return '3'
+  }
+}
+
 function click_revegetate () {
   $.mobile.loading('show')
 
@@ -203,20 +225,20 @@ function click_revegetate () {
     $.mobile.loading('hide')
     var str = ''
     for (var i = 0; i < data.plants.length; i++) {
-			 var formimg = '<img src="img/app_icons/form1_off.png">'
+      var formimg = '<img src="img/app_icons/form1_off.png">'
 
-			 if (typeof data.plants[i].form !== 'undefined' && data.plants[i].form != '') {
-   formimg = '<img src="img/app_icons/form' + data.plants[i].form + '_on.png">'
-			 }
+      if (typeof data.plants[i].type !== 'undefined' && data.plants[i].type != '') {
+        formimg = '<img src="img/app_icons/form' + getForm(data.plants[i].type) + '_on.png">'
+      }
 
-			 var starimg = '<img class="fav-icon-list" src="img/app_icons/star_off.png" onclick="add_veg_list(\'' + data.plants[i].scientificName + '\',this)"/>'
-			 for (var j = 0; j < veg_fav_list.length; j++) {
-   if (veg_fav_list[j].scientificName == data.plants[i].scientificName) {
-					 starimg = '<img class="fav-icon-list" src="img/app_icons/star_on.png" onclick="remove_veg_list(\'' + data.plants[i].scientificName + '\',this)"/>'
-   }
-			 }
+      var starimg = '<img class="fav-icon-list" src="img/app_icons/star_off.png" onclick="add_veg_list(\'' + data.plants[i].scientificName + '\',this)"/>'
+      for (var j = 0; j < veg_fav_list.length; j++) {
+        if (veg_fav_list[j].scientificName == data.plants[i].scientificName) {
+          starimg = '<img class="fav-icon-list" src="img/app_icons/star_on.png" onclick="remove_veg_list(\'' + data.plants[i].scientificName + '\',this)"/>'
+        }
+      }
 
-			 str += '<li onclick="open_detail(\'' + data.plants[i].scientificName + '\',\'list\',\'veg\')">' + formimg + '<h3>' + data.plants[i].scientificName + '</h3><p>' + data.plants[i].commonName + ' - Occurences: ' + data.plants[i].count + '</p>' + starimg + '</li>'
+      str += '<li onclick="open_detail(\'' + data.plants[i].scientificName + '\',\'list\',\'veg\')">' + formimg + '<h3>' + data.plants[i].scientificName + '</h3><p>' + data.plants[i].commonName + ' - Occurences: ' + data.plants[i].count + '</p>' + starimg + '</li>'
     }
     if (typeof data.region !== 'undefined' && data.region != '') {
       $('.region-name-header').html(' - ' + data.region.name + ' - ' + data.region.regionName + ' - ' + data.region.state)
@@ -227,7 +249,7 @@ function click_revegetate () {
     $(':mobile-pagecontainer').pagecontainer('change', '#plant-list-revegetate', { role: 'page' })
     $('#veg-plant-list').listview('refresh')
   }, 'json').fail(function (err) {
-  	console.log(err)
+    console.log(err)
     show_alert('Connection error2')
     $.mobile.loading('hide')
   })
@@ -238,15 +260,15 @@ function click_landscape_myplant () {
   $.mobile.loading('hide')
   var str = ''
   for (var i = 0; i < ls_fav_list.length; i++) {
-			 var formimg = '<img src="img/app_icons/form1_off.png">'
+    var formimg = '<img src="img/app_icons/form1_off.png">'
 
-			 if (typeof ls_fav_list[i].form !== 'undefined' && ls_fav_list[i].form != '') {
-   formimg = '<img src="img/app_icons/form' + ls_fav_list[i].form + '_on.png">'
-			 }
+    if (typeof ls_fav_list[i].type !== 'undefined' && ls_fav_list[i].type != '') {
+      formimg = '<img src="img/app_icons/form' + getForm(ls_fav_list[i].type) + '_on.png">'
+    }
 
-			 var starimg = '<img class="fav-icon-list" src="img/app_icons/star_on.png" onclick="remove_ls_list_myplant(\'' + ls_fav_list[i].scientificName + '\',this)"/>'
+    var starimg = '<img class="fav-icon-list" src="img/app_icons/star_on.png" onclick="remove_ls_list_myplant(\'' + ls_fav_list[i].scientificName + '\',this)"/>'
 
-			 str += '<li onclick="open_detail(\'' + ls_fav_list[i].scientificName + '\',\'fav\',\'ls\')">' + formimg + '<h3>' + ls_fav_list[i].scientificName + '</h3><p>' + ls_fav_list[i].commonName + ' - Occurences: ' + ls_fav_list.count + '</p>' + starimg + '</li>'
+    str += '<li onclick="open_detail(\'' + ls_fav_list[i].scientificName + '\',\'fav\',\'ls\')">' + formimg + '<h3>' + ls_fav_list[i].scientificName + '</h3><p>' + ls_fav_list[i].commonName + ' - Occurences: ' + ls_fav_list.count + '</p>' + starimg + '</li>'
   }
 
   $('#my-plan-list-list').html(str)
@@ -302,15 +324,15 @@ function click_revegetate_myplant () {
   $.mobile.loading('hide')
   var str = ''
   for (var i = 0; i < veg_fav_list.length; i++) {
-			 var formimg = '<img src="img/app_icons/form1_off.png">'
+    var formimg = '<img src="img/app_icons/form1_off.png">'
 
-			 if (typeof veg_fav_list[i].form !== 'undefined' && veg_fav_list[i].form != '') {
-   formimg = '<img src="img/app_icons/form' + veg_fav_list[i].form + '_on.png">'
-			 }
+    if (typeof veg_fav_list[i].type !== 'undefined' && veg_fav_list[i].type != '') {
+      formimg = '<img src="img/app_icons/form' + getForm(veg_fav_list[i].type) + '_on.png">'
+    }
 
-			 var starimg = '<img class="fav-icon-list" src="img/app_icons/star_on.png" onclick="remove_veg_list_myplant(\'' + veg_fav_list[i].scientificName + '\',this)"/>'
+    var starimg = '<img class="fav-icon-list" src="img/app_icons/star_on.png" onclick="remove_veg_list_myplant(\'' + veg_fav_list[i].scientificName + '\',this)"/>'
 
-			 str += '<li onclick="open_detail(\'' + veg_fav_list[i].scientificName + '\',\'fav\',\'veg\')">' + formimg + '<h3>' + veg_fav_list[i].scientificName + '</h3><p>' + veg_fav_list[i].commonName + ' - Occurences: ' + veg_fav_list.count + '</p>' + starimg + '</li>'
+    str += '<li onclick="open_detail(\'' + veg_fav_list[i].scientificName + '\',\'fav\',\'veg\')">' + formimg + '<h3>' + veg_fav_list[i].scientificName + '</h3><p>' + veg_fav_list[i].commonName + ' - Occurences: ' + veg_fav_list.count + '</p>' + starimg + '</li>'
   }
   $('#my-area-plant-info').html('Your location area will need ' + veg_plant_needed + ' plants from the following options. You will need a ratio of 1 tree to 2 shrubs to 4 groundcorvers.')
   $('#my-area-plan-list-list').html(str)
@@ -366,40 +388,40 @@ function search_plant () {
   $.mobile.loading('show')
 
   $.get(URL + 'plant/search', form_data + '&region=' + myregion + '&lat=' + mylocation[1] + '&lng=' + mylocation[0]
-	, function (data) {
-  ls_data_list = data
-  $.mobile.loading('hide')
-  var str = ''
-  for (var i = 0; i < data.plants.length; i++) {
-			 var formimg = '<img src="img/app_icons/form1_off.png">'
+  , function (data) {
+    ls_data_list = data
+    $.mobile.loading('hide')
+    var str = ''
+    for (var i = 0; i < data.plants.length; i++) {
+      var formimg = '<img src="img/app_icons/form1_off.png">'
 
-			 if (typeof data.plants[i].form !== 'undefined' && data.plants[i].form != '') {
-   formimg = '<img src="img/app_icons/form' + data.plants[i].form + '_on.png">'
-			 }
+      if (typeof data.plants[i].type !== 'undefined' && data.plants[i].type != '') {
+        formimg = '<img src="img/app_icons/form' + getForm(data.plants[i].type) + '_on.png">'
+      }
 
-			 var starimg = '<img class="fav-icon-list" src="img/app_icons/star_off.png" onclick="add_ls_list(\'' + data.plants[i].scientificName + '\',this)"/>'
-			 for (var j = 0; j < ls_fav_list.length; j++) {
-   if (ls_fav_list[j].scientificName == data.plants[i].scientificName) {
-					 starimg = '<img class="fav-icon-list" src="img/app_icons/star_on.png" onclick="remove_ls_list(\'' + data.plants[i].scientificName + '\',this)"/>'
-   }
-			 }
+      var starimg = '<img class="fav-icon-list" src="img/app_icons/star_off.png" onclick="add_ls_list(\'' + data.plants[i].scientificName + '\',this)"/>'
+      for (var j = 0; j < ls_fav_list.length; j++) {
+        if (ls_fav_list[j].scientificName == data.plants[i].scientificName) {
+          starimg = '<img class="fav-icon-list" src="img/app_icons/star_on.png" onclick="remove_ls_list(\'' + data.plants[i].scientificName + '\',this)"/>'
+        }
+      }
 
-			 str += '<li onclick="open_detail(\'' + data.plants[i].scientificName + '\',\'list\',\'ls\')">' + formimg + '<h3>' + data.plants[i].scientificName + '</h3><p>' + data.plants[i].commonName + ' - Occurences: ' + data.plants[i].count + '</p>' + starimg + '</li>'
-  }
-  if (typeof data.region !== 'undefined' && data.region != '') {
-    $('.region-name-header').html(' - ' + data.region.name + ' - ' + data.region.regionName + ' - ' + data.region.state)
-  }
-  $('#ls-plant-list').html(str)
+      str += '<li onclick="open_detail(\'' + data.plants[i].scientificName + '\',\'list\',\'ls\')">' + formimg + '<h3>' + data.plants[i].scientificName + '</h3><p>' + data.plants[i].commonName + ' - Occurences: ' + data.plants[i].count + '</p>' + starimg + '</li>'
+    }
+    if (typeof data.region !== 'undefined' && data.region != '') {
+      $('.region-name-header').html(' - ' + data.region.name + ' - ' + data.region.regionName + ' - ' + data.region.state)
+    }
+    $('#ls-plant-list').html(str)
 
-  $(':mobile-pagecontainer').pagecontainer('change', '#plant-list-landscape', { role: 'page' })
-  $('#ls-plant-list').listview('refresh')
-}, 'json').fail(function () {
-  show_alert('Connection error')
-  $.mobile.loading('hide')
-})
+    $(':mobile-pagecontainer').pagecontainer('change', '#plant-list-landscape', { role: 'page' })
+    $('#ls-plant-list').listview('refresh')
+  }, 'json').fail(function () {
+    show_alert('Connection error')
+    $.mobile.loading('hide')
+  })
 }
 function open_detail (bname, type, atype) {
-	// type: favourite or list, atype: landscape or revegetate
+  // type: favourite or list, atype: landscape or revegetate
   $.mobile.loading('show')
   if (type == 'fav') {
     if (atype == 'ls') {
@@ -428,21 +450,21 @@ function open_detail (bname, type, atype) {
 
       var formimg = '<img src="img/app_icons/form1_off.png" class="h2-logo">'
 
-      if (typeof data_plan.form !== 'undefined' && data_plan.form != '') {
-        formimg = '<img src="img/app_icons/form' + data_plan.form + '_on.png" class="h2-logo">'
+      if (typeof data_plan.type !== 'undefined' && data_plan.type != '') {
+        formimg = '<img src="img/app_icons/form' + getForm(data_plan.type) + '_on.png" class="h2-logo">'
       }
       var str = formimg + '<h2>' + data_plan.scientificName + '</h2><div class="page-intro">' + data_plan.commonName + '</div>'
       $('#plant-detail-header').html(str)
 
       var thumbnailitem = ''
-			 if (typeof data_plan.imageUrl !== 'undefined' && data_plan.imageUrl != '') {
-   thumbnailitem = '<img src="' + data_plan.imageUrl + '"/>'
-			 }
-			 $('#page-detail-thumbnail').html(thumbnailitem)
+      if (typeof data_plan.imageUrl !== 'undefined' && data_plan.imageUrl != '') {
+        thumbnailitem = '<img src="' + data_plan.imageUrl + '"/>'
+      }
+      $('#page-detail-thumbnail').html(thumbnailitem)
 
       var formitem = 'N/A'
-      if (typeof data_plan.form !== 'undefined' && data_plan.form != '') {
-        formitem = '<img src="img/app_icons/form' + data_plan.form + '_on.png"/>'
+      if (typeof data_plan.type !== 'undefined' && data_plan.type != '') {
+        formitem = '<img src="img/app_icons/form' + getForm(data_plan.type) + '_on.png"/>'
       }
       $('#page-detail-form').html(formitem)
 
@@ -459,10 +481,11 @@ function open_detail (bname, type, atype) {
       $('#page-detail-spread').html(spreaditem)
 
       var coloritem = 'N/A'
-      if (typeof data_plan.flower_colour !== 'undefined') {
+      if (typeof data_plan.flowerColor !== 'undefined') {
         coloritem = ''
-        for (var i = 0; i < data_plan.flowerColor.length; i++) {
-          coloritem += '<img src="img/app_icons/colour_' + data_plan.flowerColor + '_on.png"/>'
+        var colors = data_plan.flowerColor.split(',')
+        for (var i = 0; i < colors.length; i++) {
+          coloritem += '<img src="img/app_icons/colour_' + colors[i].toLowerCase() + '_on.png"/>'
         }
         if (coloritem == '') coloritem = 'N/A'
       }
@@ -472,7 +495,7 @@ function open_detail (bname, type, atype) {
       if (typeof data_plan.flowerTime !== 'undefined') {
         seasonitem = ''
         for (var i = 0; i < data_plan.flowerTime.length; i++) {
-          seasonitem += '<img src="img/app_icons/season_' + data_plan.flowerTime[i] + '_on.png"/>'
+          seasonitem += '<img src="img/app_icons/season_' + data_plan.flowerTime[i].toLowerCase().trim() + '_on.png"/>'
         }
         if (seasonitem == '') seasonitem = 'N/A'
       }
@@ -507,7 +530,7 @@ function show_page2 () {
 function get_square () {
   if ($('#swidth').val() != '' && $('#sheight').val() != '') {
     var sq = $('#swidth').val() * $('#sheight').val()
-		// $("#square").attr("value",);
+    // $("#square").attr("value",);
     $('#square').val(sq)
   }
 }
